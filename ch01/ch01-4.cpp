d3f86d774fdaa51d313168e52af71d300e276a98
opar <xuyaguang0630@foxmail.com>
2020-08-27 23:07:59 +0800

    finish chapter 1.2

---
+++ ch01/ch01-4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "please type two number" << std::endl;
+  int v0=0,v1=0;
+  std::cin >> v0 >> v1;
+  std::cout << "the result is " << v0 * v1 << std::endl;
+  return 0;
+}
